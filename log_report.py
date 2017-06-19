7f485d78e1273bb55e00afe28b3096ed7e3ec412
Tom Cole <colecode.ph@gmail.com>
2017-06-19 15:31:44 +0800

    Add SQL query for the second question and test in Python 2 and 3

--- log_report.py
+++ log_report.py
@@ -19,4 +19,16 @@ for row in results:
 print("")
 
 # query to answer the second question
-sql_query = ()
+sql_query = ("""SELECT authors.name, COUNT(*) AS views
+             FROM authors, log, articles
+             WHERE authors.id = articles.author
+             AND log.path LIKE '%' || articles.slug
+             GROUP BY authors.name
+             ORDER BY views DESC;""")
+cursor.execute(sql_query)
+results = cursor.fetchall()
+# print the question and answer in a human readable format
+print ("2. What are the three most popular article authors of all time?\n")
+for row in results:
+    print ('"' + row[0] + '" - ' + str(row[1]) + " views")
+print("")
