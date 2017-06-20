dcea4b23b445f70b25187f72103e63133c0d96ff
vagrant <vagrant@vagrant.vm>
2017-06-20 05:56:14 +0000

    Add third query and test

--- log_report.py
+++ log_report.py
@@ -30,5 +30,17 @@ results = cursor.fetchall()
 # print the question and answer in a human readable format
 print ("2. What are the three most popular article authors of all time?\n")
 for row in results:
-    print ('"' + row[0] + '" - ' + str(row[1]) + " views")
+    print (row[0] + ' - ' + str(row[1]) + " views")
 print("")
+
+# query to answer third question - queries the view error_requests, SEE README.md
+sql_query = ("""SELECT to_char(date, 'FMMonth FMDDth, YYYY') AS date,
+             round(errors * 100.0 / requests, 1) AS percent
+             FROM error_requests
+             WHERE (errors * 100.0 / requests) > 1;""")
+cursor.execute(sql_query)
+results = cursor.fetchall()
+# print the question and answer in a human readable format
+print ("3. On which days did more than 1% of requests lead to errors?\n")
+for row in results:
+    print (row[0] + ' - ' + str(row[1]) + "%")
