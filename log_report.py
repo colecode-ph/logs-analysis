718ba89fb8d56740e671b9dc0484f9d0e730d649
vagrant <vagrant@vagrant.vm>
2017-06-23 13:39:01 +0000

    Add print_top_authors() and print_top_error_days() functions and test

--- log_report.py
+++ log_report.py
@@ -15,7 +15,7 @@ def connect(database_name):
 
 def fetch_query(sql_query):
     ''' queries the database and returns the results '''
-    db, cursor = connect("news")
+    db, cursor = connect(DBNAME)
     cursor.execute(sql_query)
     results = cursor.fetchall()
     cursor.close()
@@ -35,35 +35,32 @@ def print_top_articles():
         print ('"' + row[0] + '" - ' + str(row[1]) + " views")
     print("")
 
+def print_top_authors():
+    ''' defines the query and prints the question and answer '''
+    sql_query = ("""SELECT authors.name, COUNT(*) AS views
+                FROM authors, log, articles
+                WHERE authors.id = articles.author
+                AND log.path LIKE '%' || articles.slug
+                GROUP BY authors.name
+                ORDER BY views DESC;""")
+    results = fetch_query(sql_query)
+    print ("2. What are the three most popular article authors of all time?\n")
+    for row in results:
+        print (row[0] + ' - ' + str(row[1]) + " views")
+    print("")
+
+def print_top_error_days():
+    ''' defines the query and prints the question and answer '''
+    sql_query = ("""SELECT to_char(date, 'FMMonth FMDDth, YYYY') AS date,
+                 round(errors * 100.0 / requests, 1) AS percent
+                 FROM error_requests
+                 WHERE (errors * 100.0 / requests) > 1;""")
+    results = fetch_query(sql_query)
+    print ("3. On which days did more than 1% of requests lead to errors?\n")
+    for row in results:
+        print (row[0] + ' - ' + str(row[1]) + "%")
+
 if __name__ == '__main__':
     print_top_articles()
-
-
-'''
-# query to answer the second question
-sql_query = ("""SELECT authors.name, COUNT(*) AS views
-             FROM authors, log, articles
-             WHERE authors.id = articles.author
-             AND log.path LIKE '%' || articles.slug
-             GROUP BY authors.name
-             ORDER BY views DESC;""")
-cursor.execute(sql_query)
-results = cursor.fetchall()
-# print the question and answer in a human readable format
-print ("2. What are the three most popular article authors of all time?\n")
-for row in results:
-    print (row[0] + ' - ' + str(row[1]) + " views")
-print("")
-
-# query to answer third question
-sql_query = ("""SELECT to_char(date, 'FMMonth FMDDth, YYYY') AS date,
-             round(errors * 100.0 / requests, 1) AS percent
-             FROM error_requests
-             WHERE (errors * 100.0 / requests) > 1;""")
-cursor.execute(sql_query)
-results = cursor.fetchall()
-# print the question and answer in a human readable format
-print ("3. On which days did more than 1% of requests lead to errors?\n")
-for row in results:
-    print (row[0] + ' - ' + str(row[1]) + "%")
-'''
+    print_top_authors()
+    print_top_error_days()
