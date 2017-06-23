cda5a40f76b187df5c51b684470169c9f2fbd7b9
vagrant <vagrant@vagrant.vm>
2017-06-23 12:41:30 +0000

    Create function to define the query and print results of the first question

--- log_report.py
+++ log_report.py
@@ -2,37 +2,57 @@ import psycopg2
 
 DBNAME = "news"
 # create the database connection and get the cursor
-def connect(database_name):
-    ''' connect to the PostgreSQL database - returns a connection '''
+'''def connect(database_name):
+    connect to the PostgreSQL database - returns a connection
     try:
         db = psycopg2.connect("dbname={}".format(database_name))
         cursor = db.cursor
         return db, cursor
     except psycopg2.Error as error:
-        print "Unable to connect to the database"
-        sys.exit(1)
-
+        print ("Unable to connect to the database")
+        sys.exit(1)'''
 
 #conn = psycopg2.connect(database=DBNAME)
 #cursor = conn.cursor()
+#connect(DBNAME)
 
-connect(news)
+def fetch_query(sql_query):
+    ''' queries the database and returns the results '''
+    db = psycopg2.connect(database=DBNAME)
+    cursor = db.cursor()
+    cursor.execute(sql_query)
+    results = cursor.fetchall()
+    cursor.close()
+    db.close()
+    return results
+
+def print_top_articles():
+    ''' defines the query and prints the question and answer '''
+    sql_query = ("""SELECT articles.title, COUNT(*) AS views
+                FROM articles, log
+                WHERE log.path LIKE '%' || articles.slug
+                GROUP BY articles.title
+                ORDER BY views DESC LIMIT 3;""")
+    results = fetch_query(sql_query)
+    print ("1. What are the three most popular articles of all time?\n")
+    for row in results:
+        print ('"' + row[0] + '" - ' + str(row[1]) + " views")
+    print("")
+
+if __name__ == '__main__':
+    print_top_articles()
 
 
-# query to answer the first question
-sql_query = ("""SELECT articles.title, COUNT(*) AS views
-             FROM articles, log
-             WHERE log.path LIKE '%' || articles.slug
-             GROUP BY articles.title
-             ORDER BY views DESC LIMIT 3;""")
-cursor.execute(sql_query)
-results = cursor.fetchall()
+#cursor.execute(sql_query)
+#results = cursor.fetchall()
 # print the question and answer in a human readable format
-print ("1. What are the three most popular articles of all time?\n")
-for row in results:
-    print ('"' + row[0] + '" - ' + str(row[1]) + " views")
-print("")
 
+
+
+
+
+
+'''
 # query to answer the second question
 sql_query = ("""SELECT authors.name, COUNT(*) AS views
              FROM authors, log, articles
@@ -59,3 +79,4 @@ results = cursor.fetchall()
 print ("3. On which days did more than 1% of requests lead to errors?\n")
 for row in results:
     print (row[0] + ' - ' + str(row[1]) + "%")
+'''
